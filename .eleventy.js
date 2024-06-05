d12f6e82901ad1eea44e629681b50686934b5ad9
Hellen Abolhassanlo <fatemeh.abolhassanlo@bulme.at>
2024-06-05 13:37:06 +0200

    toc pluging added

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,15 @@
-const Image = require("@11ty/eleventy-img");
-const shell = require('shelljs');
 
-async function imageShortcode(src, alt, sizes) {
-  let metadata = await Image(src, {
-    widths: [20, 40],
-    formats: ["avif", "jpeg"]
-  });
-
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-  }
-
-  shell.mkdir('-p', '_site/images'); // Create the directory if it doesn't exist already
-  shell.cp('images/*', '_site/images'); // Copy everything from images directory into _site/images
-
-  return Image.generateHTML(metadata, imageAttributes);
-}
-
+const pluginTOC = require('eleventy-plugin-toc');
 module.exports = function (eleventyConfig) {
 
+   eleventyConfig.addPlugin(pluginTOC , {
+    tags: ['h2', 'h3'],
+    wrapper: 'div'
+  });
+  // Pass-through copy for images
     eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addWatchTarget("images/");
 
-    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-    eleventyConfig.addLiquidShortcode("image", imageShortcode);
-    eleventyConfig.addJavaScriptFunction("image", imageShortcode);
-
+    // Set input and output directories
     return {
         dir: {
             input: "pages",
